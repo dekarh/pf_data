12a434a6e86b2c2c257f30bdf9a18087dc4264dd
dekarh <dekarh3@gmail.com>
2022-04-21 16:30:54 +0400

    Модуль pf_data, версия 0.0.17.1 от 21.04.2022:   * Поправил __manifest__.py   * Добавил README.md

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "users_pf",
+    'name': "pf_data",
 
     'summary': """
-        Импорт юзеров из ПланФикса 
+       Импорт данных из API ПланФикса для модуля docflow 
     """,
 
     'description': """
-        Импорт юзеров из ПланФикса 
+       Импорт данных из API ПланФикса для модуля docflow 
+        (документооборот для flectra в стиле Планфикс) 
     """,
 
     'author': "Денис Алексеев",
@@ -16,11 +17,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/flectra/flectra/blob/master/flectra/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Human Resources',
-    'version': '0.0.3',
+    'category': 'Sign',
+    'version': '0.0.17',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'project', 'docflow'],
 
     # always loaded
     'data': [
